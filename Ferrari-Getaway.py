e668ac3d96231f1ce95fd1f741741b12a8ea1d7f
Mikhail Ibrahim <Mikhail.ibrahim@stu.ocsb.ca>
2025-05-21 17:07:15 +0000

    Added etc

---
+++ Ferrari-Getaway.py
@@ -0,0 +1,23 @@
+# 1/usr/bin/env python3
+# Created by : Mr. Coxall
+# Created on: May 2025
+# Edited by: Mikhail Ibrahim
+# This program is the "Ferrari Getaway Game." program on the pybadge.
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n")
+    print("Hello, Ferrari Drivers!")
+
+    # repeat forever, game loop.
+    while True:
+        pass  # just the placeholder for now.
+
+
+if __name__ == "__main__":
+    game_scene()
